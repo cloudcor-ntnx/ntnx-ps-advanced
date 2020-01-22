34d63c15bbe571876a9f83600b37be7988195865
cloudcor-ntnx <55808532+cloudcor-ntnx@users.noreply.github.com>
2020-01-21 19:06:47 -0800

    Update new-container.ps1

--- new-container.ps1
+++ new-container.ps1
@@ -1,2 +1,9 @@
+	   <# 
+	   
+        .SYNOPSIS
+            Prompts for new container name and creates it with no specified parameters
+
+    #>
+
 $containername = Read-host -prompt 'Enter the Name of the Container'
 New-NTNXContainer -name $containername
