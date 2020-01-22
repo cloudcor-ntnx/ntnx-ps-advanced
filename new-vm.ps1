7fdad915d66c8d3b0147d7fce6a52464c1b2c937
cloudcor-ntnx <55808532+cloudcor-ntnx@users.noreply.github.com>
2020-01-21 19:11:47 -0800

    Update new-vm.ps1

--- new-vm.ps1
+++ new-vm.ps1
@@ -1,3 +1,11 @@
+	   <# 
+	   
+        .SYNOPSIS
+            Prompts for basic parameters for a new VM container, no disk has been added 
+
+    #>
+
+
 $VMName = Read-host -prompt 'Enter the Name of the VM'
 $vCPUs = Read-host -prompt 'Enter the # of vCPUs'
 $ramMB = Read-host -prompt 'Enter the amount of RAM in MB (example = 2048 for 2GB)'
