151180db5ee9e81061e1495efa30d2ff0fe06f66
cloudcor-ntnx <55808532+cloudcor-ntnx@users.noreply.github.com>
2020-01-21 19:13:08 -0800

    Update start-maintenancemode.ps1

--- start-maintenancemode.ps1
+++ start-maintenancemode.ps1
@@ -1,3 +1,9 @@
+	   <# 
+	   
+        .SYNOPSIS
+            Put host into maintenance mode
+    #>
+
 $hostID = Read-host -prompt 'Enter the host ID'
 $evacOption = Read-host -prompt 'Enter the VM Evacuation Option (options POWER_OFF, COLD_MIGRATE, LIVE_MIGRATE)'
 start-ntnxmaintenancemode -hostID $hostID -evacuationoption $evacOption 
