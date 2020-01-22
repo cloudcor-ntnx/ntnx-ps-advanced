b98c72ca79f99495e6edd7cc09fb9108061b1a67
cloudcor-ntnx <55808532+cloudcor-ntnx@users.noreply.github.com>
2020-01-21 19:07:56 -0800

    Update new-network.ps1

--- new-network.ps1
+++ new-network.ps1
@@ -1,3 +1,10 @@
+	   <# 
+	   
+        .SYNOPSIS
+            Prompts for vlan name and vlan ID's of a new network 
+
+    #>
+
 $networkName = Read-host -prompt 'Enter the Name of the Network'
 $networkVLANID = Read-host -prompt 'Enter the VLANID'
 New-ntnxNetwork -name $networkName -vlanid $networkVLANID
