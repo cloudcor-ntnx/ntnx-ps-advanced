53233885bb03396cf498de3fdd408df6dd27580f
cloudcor-ntnx <55808532+cloudcor-ntnx@users.noreply.github.com>
2020-09-14 16:12:58 -0700

    Update new-vm.ps1

--- new-vm.ps1
+++ new-vm.ps1
@@ -9,4 +9,5 @@
 $VMName = Read-host -prompt 'Enter the Name of the VM'
 $vCPUs = Read-host -prompt 'Enter the # of vCPUs'
 $ramMB = Read-host -prompt 'Enter the amount of RAM in MB (example = 2048 for 2GB)'
-New-NTNXVirtualMachine -Name $VMName -NumVcpus $vCPUs -MemoryMb $ramMB
+$guestOS = Read-host -promt 'Enter the guestOS' 
+New-NTNXVirtualMachine -Name $VMName -NumVcpus $vCPUs -MemoryMb $ramMB -guestOS $guestOS
